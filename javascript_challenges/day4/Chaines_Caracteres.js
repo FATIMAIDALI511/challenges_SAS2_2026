55257bd33372092b977f88cb2c1e0dcceddbd315
FatimaIDAli <fatifatim00511@gmail.com>
2026-09-22 15:48:04 +0100

    chaine caracter

---
+++ javascript_challenges/day4/Chaines_Caracteres.js
@@ -0,0 +1,31 @@
+
+//challenge 1;
+/*
+let str = "hello world";
+let compterCaracteres=()=>{
+    let i=  0 ;
+    while(str[i]!=undefined){
+        i++;
+    }
+   
+    console.log(i);
+
+}
+compterCaracteres()
+
+//challenge 2:
+chaine="hello world"
+litre="l";
+compterLettre= (chaine, letre)=>{
+    let com=0;
+    for (let i=0; i< chaine.length; i++){
+        if(chaine[i]==letre){
+            com++;
+        }
+    }
+return com;
+
+}
+console.log("le nombre de ", litre, "dans",chaine, "est", compterLettre(chaine,litre))
+*/
+
